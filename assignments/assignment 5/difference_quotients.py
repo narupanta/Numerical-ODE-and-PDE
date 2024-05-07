9cf48e93ac5b3e923fffb76ea36c2ecc120cb0a7
PatleKshitij <kshitijpatle7@gmail.com>
2024-05-07 15:57:03 +0200

    5.1_wip-todo-replace for with linspace

--- assignments/assignment 5/difference_quotients.py
+++ assignments/assignment 5/difference_quotients.py
@@ -25,12 +25,28 @@ f_curve = func_x(x_axis)
 #Equation of a straight line: y = mx + c
 f_tangent_curve = derivative_func_x(x0) * (x_axis - x0) + func_x(x0)
 
-plt.figure()
-plt.plot( x_axis,f_curve)
-plt.plot( x_axis,f_tangent_curve,'r')
-plt.show()
+# plt.figure()
+# plt.plot( x_axis,f_curve)
+# plt.plot( x_axis,f_tangent_curve,'r')
+# plt.show()
 
-print("Forward Approximation is = ",forward_diff_quot(func_x,x0,h))
-print("Backward Approximation is = ",backward_diff_quot(func_x,x0,h))
-print("Central Approximation is = ",central_diff_quot(func_x,x0,h))
-print("Calulated value is = ",derivative_func_x(x0))
+# print("Forward Approximation is = ",forward_diff_quot(func_x,x0,h))
+# print("Backward Approximation is = ",backward_diff_quot(func_x,x0,h))
+# print("Central Approximation is = ",central_diff_quot(func_x,x0,h))
+# print("Calulated value is = ",derivative_func_x(x0))
+
+step_h = []
+forward_error = []
+backward_error = []
+central_error = []
+for i in range(-1,-6,-1):
+    step_h.append(10**i)
+    forward_error.append(derivative_func_x(x0) - forward_diff_quot(func_x,x0,10**i))
+    backward_error.append(derivative_func_x(x0) - backward_diff_quot(func_x,x0,10**i))
+    central_error.append(derivative_func_x(x0) - central_diff_quot(func_x,x0,10**i))
+
+plt.figure()
+plt.semilogy(forward_error, step_h)
+plt.semilogy(backward_error,step_h,'r')
+plt.semilogy(central_error,step_h,'g')
+plt.show()
